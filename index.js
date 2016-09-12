7b793950d4fccf1e89d7bb6e4201af9343c738f3
Viktor Larsson <viktor@wemadeyoulook.at>
2016-09-12 09:29:40 +0200

    Fixes #10, made a rollback to a previous version due to a pull request that was unverified

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var url = require('url');
 var es = require('event-stream');
 var iconv = require('iconv-lite');
 
-module.exports = function (filePath) {
+module.exports = function(filePath) {
 
-    var go = function (file, callback) {
+    var go = function(file, callback) {
 
         var markup = iconv.decode(file.contents, 'utf-8');
 
-        if (markup.indexOf('�') > -1) {
+        if(markup.indexOf('�') > -1){
             markup = iconv.decode(file.contents, 'gbk');
             markup = iconv.encode(markup, 'utf-8');
         }
@@ -25,10 +25,11 @@ module.exports = function (filePath) {
     return es.map(go);
 
     function injectSvg(dom) {
+
         // Regexp for checking if the file ending has .svg
         var testSvg = /^.*.(svg)$/i;
 
-        dom('img').each(function (idx, el) {
+        dom('img').each(function(idx, el) {
             el = dom(el)
             var src = el.attr('src');
 
@@ -40,26 +41,24 @@ module.exports = function (filePath) {
 
                 try {
 
-                    filePath = ( filePath.slice(-1) == '/' ) ? filePath : filePath + '/';
+                  var inlineTag = fs.readFileSync("./" + src).toString();
+                  var className = el.attr('class');
+                  var styles = el.attr('style');
 
-                    var inlineTag = fs.readFileSync("./" + filePath + src).toString();
-                    var className = el.attr('class');
-                    var styles = el.attr('style');
+                  svg = dom(inlineTag);
 
-                    svg = dom(inlineTag);
+                  if(className !== undefined) {
+                    svg.addClass(className);
+                  }
 
-                    if (className !== undefined) {
-                        svg.addClass(className);
-                    }
-
-                    if (styles !== undefined) {
-                        svg.attr('style', styles);
-                    }
+                  if(styles !== undefined) {
+                    svg.attr('style', styles);
+                  }
 
 
                 } catch (e) {
 
-                    svg = "<!-- File " + src + " was not found by gulp-inject-svg  -->";
+                  svg = "<!-- File "+ src + " was not found by gulp-inject-svg  -->";
 
                 }
 
